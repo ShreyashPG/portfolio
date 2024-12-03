1ecc8401edd78cdd6ff04c872aa9dc2f69e90b82
Shreyash Ghanekar <shreyasghanekar35@gmail.com>
2024-12-03 13:31:34 +0530

    Added projects

--- config/projects.ts
+++ config/projects.ts
@@ -2,10 +2,30 @@ import { ProjectItemProps } from "@/components/projects"
 
 
 export const projectsConfig :ProjectItemProps[]  =[
-
+     
+  {
+    title: 'GitHub Repository Management Tool',
+    date: ' August 2024 - September 2024',
+    techstack: 'Bash , GitHub API, AWS EC2, Linux',
+    descriptions: [
+      "Developed and deployed a GitHub Repository Management Tool on AWS EC2, utilizing Bash scripting and the GitHub API to manage repositories and user data efficiently." ,
+      "The tool supports user authentication, access control, project tracking, workflow management, and pull request monitoring. It includes error handling, retry mechanisms, and dynamic user prompts for a seamless experience.",
+      "The project simplifies repository and user interaction management on GitHub, offering a comprehensive solution for developers and repository administrators.", 
+    ],
+  },
+  {
+    title: 'Huffman Compressor',
+    date: ' August 2023',
+    techstack: 'JAVA , Huffman Encoding, Google Guava ',
+    descriptions: [
+      "Developed a Java-based text compression tool utilizing the Huffman encoding scheme for efficient file compression." ,
+      "The tool compresses text files into a .cmp format using Huffman encoding and decodes them back to .txt format, producing the compressed text of the original file.",
+      "Utilizes Google Guava for efficient bi-directional mapping and UTF-8 encoding for reliable text compression and decompression.", 
+    ],
+  },
     {
         title: 'Scheduler',
-        date: ' August 2024 - September 2024',
+        date: ' August 2024',
         techstack: 'React.js ',
         descriptions: [
           "Web-based scheduler application built in React.js that allows users to visualize and analyze various CPU scheduling" ,
@@ -14,18 +34,18 @@ export const projectsConfig :ProjectItemProps[]  =[
         ],
       },
       {
-        title: 'Secret Pass',
-        date: ' February 2024 - March 2024',
-        techstack: 'Typescript, MongoDB, Prisma',
+        title: 'Shadow Secret',
+        date: ' February 2024',
+        techstack: 'Full Stack Project',
         descriptions: [
-          "Developed a secure and encrypted text-sharing web application using TypeScript, MongoDB, and Prisma" ,
+          "Developed a secure and encrypted text-sharing web application using TypeScript,Tailwind, MongoDB, and Prisma" ,
           "The tool ensures confidential communication by encrypting messages with a user-provided password and securely storing them. Users can set expiration times for messages, which automatically delete after the specified duration.",
           "Used AES-256 encryption for messages using Crypto module for Secure storage and expiration mechanism for secret messages. ", 
         ],
       },
       { 
         title: 'Social Media App',
-        date: 'December 2023 - January 2024',
+        date: 'December 2023',
         techstack: 'MERN Stack',
         descriptions: [
           "Developed a full-stack social media application using the MERN stack, incorporating modern frontend and backend technologies." ,
