5ea3db09e63e095a3c028247f372ea2368ab42fd
Shreyash Ghanekar <shreyasghanekar35@gmail.com>
2025-06-18 16:12:34 +0530

    update work experience

--- config/work-experience.ts
+++ config/work-experience.ts
@@ -1,21 +1,9 @@
 import { WorkExperienceItemProps } from '@/components/work-experience-item';
 
 export const workExperienceConfig: WorkExperienceItemProps[] = [
-  {
-    company: 'Pune Institute of Computer Technology',
-    title: 'Frontend Developer and Database Administrator ',
-    date: 'April 2024 - October 2024',
-    location: 'I.T Department , P.I.C.T Pune',
-     descriptions: [
-      "Architected a React.js frontend for a student/teacher data portal, serving 250+ users." ,
-      "Wrote 25+ optimized SQL queries to enhance data retrieval performance. Also implemented role-based access control and encrypted data transfers to safeguard sensitive PICT data.",
-      "Successfully deployed and maintained the project for over 6 months along with integrating new requirements.", 
-    ],
-   
-  },
-  {
+   {
     company: 'Borgave Group',
-    title: 'Backend Developer ',
+    title: 'Backend Developer',
     date: 'January 2025 -  March 2025',
     location: 'Borgave Group',
    
@@ -24,6 +12,19 @@ export const workExperienceConfig: WorkExperienceItemProps[] = [
       "Designed and implemented MongoDB data models for scalable storage and efficient access.",
       "Integrated JWT authentication and cookie-based session management to secure 50+ daily logins.", 
     ],
+  },
+  {
+    company: 'Pune Institute of Computer Technology',
+    title: 'Full Stack Developer Intern',
+    date: 'April 2024 - October 2024',
+    location: 'I.T Department , P.I.C.T Pune',
+     descriptions: [
+      "Architected a React.js frontend for a student/teacher data portal, serving 250+ users." ,
+      "Wrote 25+ optimized SQL queries to enhance data retrieval performance. Also implemented role-based access control and encrypted data transfers to safeguard sensitive PICT data.",
+      "Successfully deployed and maintained the project for over 6 months along with integrating new requirements.", 
+    ],
+   
   }
+ 
 
 ];
